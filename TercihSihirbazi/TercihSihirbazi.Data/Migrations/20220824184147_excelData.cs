b7c9240dc20c5826212a4479eef4d0f3af7b3c4a
kadirkurhan <kadirkurhan42@hotmail.com>
2022-08-24 21:46:47 +0300

    add cors

---
+++ TercihSihirbazi/TercihSihirbazi.Data/Migrations/20220824184147_excelData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TercihSihirbazi.Data.Migrations
+{
+    public partial class excelData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
